9c05e8e72c4e4100a91855612f764f4d5a3b0a50
adi303330 <adityadhankhar24@gmail.com>
2025-06-03 16:53:44 +0530

    Configure base path in Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // No 'plugins: [react()]' if it's not a React project
+      base: '/Leo-Design/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
